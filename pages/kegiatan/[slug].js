ac6f576785e826f91d716f737a828d70f45f4eec
Av4nz <cinxzaned2003@gmail.com>
2025-08-15 00:38:56 +0700

    first commit

---
+++ pages/kegiatan/[slug].js
@@ -0,0 +1,45 @@
+import fs from "fs";
+import path from "path";
+import matter from "gray-matter";
+import { remark } from "remark";
+import html from "remark-html";
+import Image from "next/image";
+
+export async function getStaticPaths() {
+    const files = fs.readdirSync("contents/kegiatan")
+    const paths = files.map((filename) => ({
+        params: { slug: filename.replace(".md", "")},
+    }))
+    return { paths, fallback: false }
+}
+
+export async function getStaticProps({ params }) {
+  const filePath = path.join("contents/kegiatan", `${params.slug}.md`);
+  const fileContent = fs.readFileSync(filePath, "utf8");
+  const { data, content } = matter(fileContent);
+  const processedContent = await remark().use(html).process(content);
+  return {
+    props: {
+      frontmatter: {
+        ...data,
+        date: new Date(data.date).toISOString(),
+      },
+      content: processedContent.toString(),
+    },
+  };
+}
+
+export default function Kegiatan({ frontmatter, content }) {
+  return (
+    <div className="p-6">
+      <h1 className="text-2xl font-bold">{frontmatter.title}</h1>
+      <p className="text-sm text-gray-600">{frontmatter.date}</p>
+      <Image
+        src={frontmatter.thumbnail}
+        alt={frontmatter.title}
+        className="my-4"
+      />
+      <div dangerouslySetInnerHTML={{ __html: content }} />
+    </div>
+  );
+}
