fb1610258a1dcaea5d884e497fe281dc2fb43279
Av4nz <cinxzaned2003@gmail.com>
2025-08-24 23:22:26 +0700

    Update next.config

--- next.config.mjs
+++ next.config.mjs
@@ -3,11 +3,13 @@ import withFlowbiteReact from "flowbite-react/plugin/nextjs";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-};
-
-module.exports = {
   images: {
-    domains: ["res.cloudinary.com"],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
   },
 };
 
